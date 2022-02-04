30404ac296d985f2b7c65eb42c47204d177e39f0
endisl <endiluamba@gmail.com>
2022-02-03 23:23:13 -0300

    Updated app component

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -17,3 +17,40 @@ class App extends React.Component {
         )
     }
 }
+
+class EmployeeList extends React.Component{
+    render() {
+        const employees = this.props.employees.map(employee =>
+            <Employee key={employee._links.self.href} employee={employee}/>
+        );
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Description</th>
+                </tr>
+                {employees}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class Employee extends React.Component{
+    render() {
+        return (
+            <tr>
+                <td>{this.props.employee.firstName}</td>
+                <td>{this.props.employee.lastName}</td>
+                <td>{this.props.employee.description}</td>
+            </tr>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
