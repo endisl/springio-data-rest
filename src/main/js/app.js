0fbfaf81636466e7cc3a3b17f3ed52a949afc83e
endisl <endiluamba@gmail.com>
2022-02-03 22:55:08 -0300

    Created react app component

---
+++ src/main/js/app.js
@@ -0,0 +1,19 @@
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {employees: []};
+    }
+
+    componentDidMount() {
+        client({method: 'GET', path: '/api/employees'}).done(response => {
+            this.setState({employees: response.entity._embedded.employees});
+        });
+    }
+
+    render() {
+        return (
+            <EmployeeList employees={this.state.employees}/>
+        )
+    }
+}
