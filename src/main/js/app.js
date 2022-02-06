1d945a1ad68f40a621182d2c6e566bf5dd67c51a
endisl <endiluamba@gmail.com>
2022-02-05 22:33:06 -0300

    Created UI controller files

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,3 +1,8 @@
+'use strict'
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
+
 class App extends React.Component {
 
     constructor(props) {
